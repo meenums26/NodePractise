73964daf372d3e8df3fd8903f07826261241f203
Meenu M S <meenums@Meenus-MacBook-Air.local>
2024-07-08 22:17:02 +0530

    new problems

--- practise.js
+++ practise.js
@@ -63,3 +63,70 @@ function factorial(number){
     
 }
 
+// 07 function to check if a given number is prime. 
+
+function isPrime(num){
+    if(num <=1){
+        return false
+    }
+    if(num <=3){
+        return true
+    }
+    for(let i=2;i<Math.sqrt(num);i++){
+        if(num%i ===0){
+            return false
+        }
+        return true
+    }
+}
+
+//08 find the largest element in a nested array. 
+
+function nestedLarge(nestedArr){
+    let large = nestedArr[0][0];
+    for(let arr of nestedArr){
+        for(let num of arr){
+            if (num > large){
+                large = num;
+            }
+        }
+    }
+    return large
+}
+//09 function that returns the Fibonacci sequence up to a given number of terms. 
+function fibonacciSequence(numTerms) { 
+
+    if (numTerms <= 0) return []; 
+  
+    if (numTerms === 1) return [0]; 
+  
+     
+  
+    let sequence = [0, 1]; 
+  
+    while (sequence.length < numTerms) { 
+  
+      let nextNumber = sequence[sequence.length - 1] + sequence[sequence.length - 2]; 
+  
+      sequence.push(nextNumber); 
+  
+    } 
+  
+    return sequence; 
+  
+  } 
+
+  // 10 program to convert a string to title case (capitalize the first letter of each word). 
+
+function makeFirstCap(str){
+    // let strArr = str.split(" ")
+    // let newArr = []
+    // for(let i =0;i<strArr.length;i++){
+    //    let newWord = strArr[i].charAt(0).toUpperCase() + strArr[i].slice(1)
+    //    newArr.push(newWord)
+    // }
+    // return newArr.join(" ")
+    return str.replace(/\b\w/g, l => l.toUpperCase()); 
+
+
+}
